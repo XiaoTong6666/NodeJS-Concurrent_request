3d9dc10cc0add277ef4d53c7093f014301cee8cd
XiaoTong6666 <110387028+XiaoTong6666@users.noreply.github.com>
2023-07-08 02:13:09 +0800

    Update 22.js
    更新了命令行参数的传递，为主程序更新命令行参数奠定基础

--- 22.js
+++ 22.js
@@ -1,13 +1,31 @@
 const { spawn } = require('child_process');
-
 function startApp() {
-  const app = spawn('node', ['2.js'], { stdio: 'inherit' });
-
-  app.on('exit', (code) => {
-    // 应用程序退出时重新启动
-	console.log('重新启动');
-    startApp();
-  });
+	const argv = process.argv.slice(2);
+	const params = {};
+	let currentParam = null;
+	for (let i = 0; i < argv.length; i++) {
+		if (argv[i].startsWith('-')) {
+			const paramName = argv[i].substring(1);
+			params[paramName] = argv[i + 1];
+			currentParam = paramName;
+			i++;
+			} else if (currentParam) {
+			params[currentParam] = argv[i];
+			currentParam = null;
+		}
+	}
+	const app = spawn('node', ['2.js', ...generateArgs(params)], { stdio: 'inherit' });
+	app.on('exit', (code) => {
+		console.log('重新启动');
+		startApp();
+	});
 }
-
+function generateArgs(params) {
+	const args = [];
+	for (const param in params) {
+		args.push(`-${param}`);
+		args.push(params[param]);
+	}
+	return args;
+}
 startApp();
